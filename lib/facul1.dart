827725f0852b96c128966d6aad2b6ad84253754a
3DS <3ds@bglab.local>
2024-09-19 10:38:07 -0300

    Criando botões na tela USP

--- lib/facul1.dart
+++ lib/facul1.dart
@@ -13,18 +13,16 @@ class Facul1 extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       backgroundColor: const Color(0xfffcfefe),
-      body: Container(
-        alignment: Alignment.center, // Alinha o conteúdo ao centro
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.zero, // Removendo o espaçamento interno
+      body: SingleChildScrollView( // Adicionando SingleChildScrollView
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.zero,
                 alignment: Alignment.topCenter,
                 width: 460.0,
-
-                // Alinha o conteúdo ao centro
                 child: Column(
                   children: [
                     const SizedBox(height: 40),
@@ -36,7 +34,7 @@ class Facul1 extends StatelessWidget {
                         color: Color.fromARGB(255, 0, 0, 0),
                       ),
                     ),
-                    const SizedBox(height: 20), // Adicionando um espaço entre os textos
+                    const SizedBox(height: 40),
                     const Text(
                       'Criada em 1934, USP é uma universidade pública, mantida pelo Estado de São Paulo e ligada à Secretaria de Ciência, Tecnologia e Inovação. Sua graduação é formada por 183 cursos, dedicados a todas as áreas do conhecimento, distribuídos em 42 unidades de ensino e pesquisa, com mais de 58 mil alunos. A pós-graduação é composta por 239 programas com cerca de 30 mil matriculados. Atualmente, é responsável por mais de 20% da produção científica brasileira. A USP conta com diversos campi, distribuídos pelas cidades de São Paulo, Bauru, Lorena, Piracicaba, Pirassununga, Ribeirão Preto, Santos, São Carlos, além de unidades de ensino, museus e centros de pesquisa.',
                       textAlign: TextAlign.justify,
@@ -45,15 +43,15 @@ class Facul1 extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    const SizedBox(height: 30.0),
+                    const SizedBox(height: 40.0),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Container(
                           width: 200.0,
-                          margin: const EdgeInsets.only(right: 5.0), // Espaço entre as imagens
+                          margin: const EdgeInsets.only(right: 5.0),
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20.0), // Borda arredondada
+                            borderRadius: BorderRadius.circular(20.0),
                             child: Image.asset(
                               'assets/img/usp.png',
                             ),
@@ -61,32 +59,100 @@ class Facul1 extends StatelessWidget {
                         ),
                         Container(
                           width: 200.0,
-                          margin: const EdgeInsets.only(left: 5.0), // Espaço entre as imagens
+                          margin: const EdgeInsets.only(left: 55.0),
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20.0), // Borda arredondada
+                            borderRadius: BorderRadius.circular(20.0),
                             child: Image.asset(
-                              'assets/img/campus1.jpg', // Substitua com o caminho da sua nova imagem
+                              'assets/img/campus1.jpg',
                             ),
                           ),
                         ),
                       ],
                     ),
-                    Container(),
+                    const SizedBox(height: 40.0),
+                    // Adicionando os botões aqui
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 300, // Largura fixa dos botões
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Ação do primeiro botão
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black, // Cor de fundo
+                              foregroundColor: Colors.white, // Cor do texto
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, // Aumentando a altura
+                                horizontal: 30.0,
+                              ),
+                              textStyle: const TextStyle(
+                                fontSize: 25.0, // Tamanho da fonte
+                              ),
+                            ),
+                            child: const Text('Site - USP'),
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        SizedBox(
+                          width: 300, // Largura fixa dos botões
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Ação do segundo botão
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black, // Cor de fundo
+                              foregroundColor: Colors.white, // Cor do texto
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, // Aumentando a altura
+                                horizontal: 30.0,
+                              ),
+                              textStyle: const TextStyle(
+                                fontSize: 25.0, // Tamanho da fonte
+                              ),
+                            ),
+                            child: const Text('Localização'),
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        SizedBox(
+                          width: 300, // Largura fixa dos botões
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Ação do terceiro botão
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black, // Cor de fundo
+                              foregroundColor: Colors.white, // Cor do texto
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, // Aumentando a altura
+                                horizontal: 30.0,
+                              ),
+                              textStyle: const TextStyle(
+                                fontSize: 25.0,
+                                fontWeight: FontWeight.bold // Tamanho da fonte
+                              ),
+                            ),
+                            child: const Text('Cursos'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: Container(
-        color: Color.fromARGB(255, 0, 0, 0), // Cor igual a da AppBar
+        color: Color.fromARGB(255, 0, 0, 0),
         padding: const EdgeInsets.all(20.0),
         child: IconButton(
           icon: const Icon(
             home,
             color: Colors.white,
-            size: 36.0, // Tamanho do ícone
+            size: 36.0,
           ),
           onPressed: () {
             Navigator.pop(context);
